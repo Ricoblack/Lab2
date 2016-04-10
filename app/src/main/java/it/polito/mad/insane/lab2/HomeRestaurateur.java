bf43488ad037752926fba5c24d3262545b49555f
Renato Carella <renato.kinze@hotmail.it>
2016-04-10 22:12:27 +0200

    - prima modifica layout grafico home page

--- app/src/main/java/it/polito/mad/insane/lab2/HomeRestaurateur.java
+++ app/src/main/java/it/polito/mad/insane/lab2/HomeRestaurateur.java
@@ -1,6 +1,9 @@
 package it.polito.mad.insane.lab2;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
@@ -8,13 +11,28 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
+
+import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class HomeRestaurateur extends AppCompatActivity
 {
@@ -48,6 +66,14 @@ public class HomeRestaurateur extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        editGraph();
+
+        setUpRecyclerView();
+
+
+    }
+
+    private void editGraph() {
         GraphView graph = (GraphView) findViewById(R.id.graph);
         BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
                 new DataPoint(0, 1),
@@ -58,7 +84,37 @@ public class HomeRestaurateur extends AppCompatActivity
                 new DataPoint(5, 1)
         });
         graph.addSeries(series);
-        series.setSpacing(10);
+        series.setSpacing(20);
+        series.setTitle("Bookings");
+        series.setDrawValuesOnTop(true);
+
+        TypedValue typedValue = new TypedValue();
+        Resources.Theme theme = HomeRestaurateur.this.getTheme();
+        theme.resolveAttribute(R.attr.colorPrimary, typedValue, true);
+        int color = typedValue.data;
+
+        series.setValuesOnTopColor(color);
+        graph.getGridLabelRenderer().setPadding(16);
+
+        graph.getViewport().setScrollable(true);
+
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(HomeRestaurateur.this, "Series1: On Data Point clicked: " + dataPoint, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        graph.getGridLabelRenderer().setHorizontalLabelsColor(color);
+        graph.getGridLabelRenderer().setVerticalLabelsColor(color);
+
+        graph.getGridLabelRenderer().setGridColor(color); //NON FUNZIONA
+
+        NumberFormat nf = NumberFormat.getInstance();
+        nf.setMaximumFractionDigits(0);
+        nf.setMaximumIntegerDigits(2);
+
+        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter(nf, nf));
 
 //        Calendar c = Calendar.getInstance();
 //        SimpleDateFormat df = new SimpleDateFormat("dd-MM");
@@ -71,25 +127,38 @@ public class HomeRestaurateur extends AppCompatActivity
         graph.getViewport().setMaxX(12); //TODO settare l'orario di chiusura
         graph.getViewport().setScrollable(true);
 
+//        // use static labels for horizontal and vertical labels
+//        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+//        staticLabelsFormatter.setHorizontalLabels(new String[]{"old", "middle", "new"});
+//        staticLabelsFormatter.setVerticalLabels(new String[]{"low", "middle", "high"});
+//        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+
+//        graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+//            @Override
+//            public String formatLabel(double value, boolean isValueX) {
+//                if (isValueX) {
+//                    // show normal x values
+//                    return super.formatLabel(value, isValueX);
+//                } else {
+//                    // show currency for y values
+//                    return super.formatLabel(value, isValueX) + " €";
+//                }
+//            }
+//        });
+
 //        graph.getGridLabelRenderer().setVerticalAxisTitle("Bookings");
 //        graph.getGridLabelRenderer().setHorizontalAxisTitle("Hours");
 
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+//        graph.getLegendRenderer().setBackgroundColor();
 
 
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
-
-        setUpRecyclerView();
-
-
+        graph.getLegendRenderer().setTextColor(Color.WHITE);
+//        graph.getLegendRenderer().setTextSize();
+//        graph.getLegendRenderer().setWidth();
+//        graph.getLegendRenderer().setBackgroundColor(color);
     }
 
     public void showDatePickerDialog(View v)
