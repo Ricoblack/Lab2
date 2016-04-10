5e6e63758c0e272c286ef09f6e5e3bc53a965bca
Federico <f.vibrati@gmail.com>
2016-04-10 18:57:04 +0200

    nuova activity ViewBooking con relativo layout, e gestione click cardView prenotazioni

---
+++ app/src/main/java/it/polito/mad/insane/lab2/ViewBooking.java
@@ -0,0 +1,13 @@
+package it.polito.mad.insane.lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_booking);
+    }
+}
