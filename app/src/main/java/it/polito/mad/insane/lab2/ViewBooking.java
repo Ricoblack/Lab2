f181f59f31ca409b95d308fcad38c39a02ec1198
Federico <f.vibrati@gmail.com>
2016-04-11 18:09:10 +0200

    nuova activity ViewBooking con relativo layout, e gestione click cardView prenotazioni

--- app/src/main/java/it/polito/mad/insane/lab2/ViewBooking.java
+++ app/src/main/java/it/polito/mad/insane/lab2/ViewBooking.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 public class ViewBooking extends AppCompatActivity {
+
+    private Booking currentBooking = null;
+    TextView note = null;
+    TextView data = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +25,40 @@ public class ViewBooking extends AppCompatActivity {
         setContentView(R.layout.activity_view_booking);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        this.currentBooking = (Booking) getIntent().getSerializableExtra("Booking");
+        this.note = (TextView)findViewById(R.id.note_dish);
+        this.data = (TextView)findViewById(R.id.date_booking);
+
+
+        setTitle("Prenotazione");
+        this.note.setText(this.currentBooking.getNote());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm");
+        this.data.setText(dateFormat.format(this.currentBooking.getDate_time().getTime()));
+
+
+        // initialize Recycler View
+        setupDishesRecyclerView();
+
+    }
+
+
+    /* Our Methods */
+    private void setupDishesRecyclerView()
+    {
+        // set Adapter
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.MenuRecyclerView);
+        DishesRecyclerAdapter adapter = new DishesRecyclerAdapter(this, currentBooking.getDishes());
+        recyclerView.setAdapter(adapter);
+
+        // set Layout Manager
+        LinearLayoutManager mLinearLayoutManagerVertical = new LinearLayoutManager(this);
+        mLinearLayoutManagerVertical.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(mLinearLayoutManagerVertical);
+
+        // set Animator
+        recyclerView.setItemAnimator(new DefaultItemAnimator()); // default animations
     }
 
 }
